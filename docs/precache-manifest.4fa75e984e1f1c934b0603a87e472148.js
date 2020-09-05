afdfef10b8d3e071fe5eb49aa99739e7a97d3ff3
Jose Antony <joseantonymundacaangeles@gmail.com>
2020-09-05 09:18:22 -0500

    docs add

---
+++ docs/precache-manifest.4fa75e984e1f1c934b0603a87e472148.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c41324c98b02dd105fdac29c77674018",
+    "url": "/index.html"
+  },
+  {
+    "revision": "576dd6a3d78c60c57b57",
+    "url": "/static/css/main.56bdcc89.chunk.css"
+  },
+  {
+    "revision": "56d90f2604cd2e9cf340",
+    "url": "/static/js/2.8c5d5c35.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8c5d5c35.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "576dd6a3d78c60c57b57",
+    "url": "/static/js/main.37ecfb86.chunk.js"
+  },
+  {
+    "revision": "42ca63168b8c659a47df",
+    "url": "/static/js/runtime-main.878f8912.js"
+  }
+]);
